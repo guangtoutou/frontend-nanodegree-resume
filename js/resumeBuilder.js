a86e1ab606e0721884017040964a9671511ec516
guangtoutou <ni.ningning@gmail.com>
2017-01-08 23:21:04 +0800

    add Name and Role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var formattedName = HTMLheaderName.replace("%data%", "James Ni");
+ var formattedRole = HTMLheaderRole.replace("%data%", "IT Manager");
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
